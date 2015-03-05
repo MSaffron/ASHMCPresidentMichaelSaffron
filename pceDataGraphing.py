bd1e929c7e7c062cd0285dab19fb05d244a23178
AriHC <Ari.HausmanCohen@gmail.com>
2015-03-05 14:07:25 -0800

    Some code updates. Seems to work

--- pceDataGraphing.py
+++ pceDataGraphing.py
@@ -3,24 +3,30 @@ import csv
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+aliases = {
+  "xXx" : "xxx",
+  "ye olde zzz" : "zzz",
+  "z" : "zzz"
+}
+
 serviceNames = [
-    "xxx",
-    "yyy",
-    "zzz"]
+    "Income after taxes",
+    "Maintenance, repairs, insurance, other expenses",
+    "Utilities, fuels, and public services",
+    "Household operations",
+    "Maintenance and repairs",
+    "Health insurance",
+    "Medical services",
+    "Personal care products and services",
+    "Personal insurance and pensions"
+    ]
+
 incomeName = "Income after taxes"    
 firstYear = 2005
-lastYear = 2013
+lastYear = 2011
 txtFirstYear = 1984
 txtLastYear = 2004
-
 allYears = range(firstYear, lastYear+1)
-
-def getFieldValue(fieldName, year, pceData):
-  for name in aliases[fieldName]:
-    if pceData[year].has_key(name):
-      return pceData[year][name]
-  # TODO: error
-  return []
 
 def getVal(item):
     return item.value
@@ -40,7 +46,7 @@ def loadTxtPCEData():
 
 def loadPCEData():
     pceData = {}
-    for year in range(firstYear, lastYear+1):
+    for year in allYears:
         pceData[year] = {}
         dataBook = xlrd.open_workbook(filename = ("data/quintile%d.xls" % year))
         if len(dataBook.sheets()) > 1:
@@ -49,72 +55,49 @@ def loadPCEData():
         allSheets = dataBook.sheets()
         sheet = allSheets[0]
         
-        # Expected format: row 0 has table title, row 1 is empty, row 2 has headers
-        columnHeaders = map(getVal, sheet.row(2)[1:]) # currently unused
-        for rowIndex in range(2,sheet.nrows): 
-        # Going from 2 on means we'll have the headers as a column
+        for rowIndex in range(sheet.nrows): 
             row = sheet.row(rowIndex)
             item = row[0].value.strip()
             values = map(getVal, row[1:])
-            if item == '' or len(row[1:]) == 0:
-                # skip empty rows/dataless rows
+            if item not in serviceNames and item not in aliases:
+                # skip non-service rows
                 continue
 
-            
+            serviceName = item
+            if aliases.has_key(item):
+              serviceName = aliases[item]
+
             # 0 = all quintiles
             # 1 = Bottom 20%
             # ... 
             # 5 = Top 20%
-            pceData[year][item] = values
-    return pceData
+            pceData[year][serviceName] = values
 
-# pceData is a dictionary of with year keys and dictionary values. 
-# The dictionary values are dictionaries from fields to a list of values.
+    return pceData
+    
+def percentifyField(field, group, data):
+  income = data[group][incomeName]
+  fieldData = data[group][field]
+  result = []
+  for i in range(len(income)):
+    result.append(fieldData[i]/income[i])
+  return result
+
 def plotData(field, pceData):
     print "Plotting", field
-    data = {}
-    groups = pceData[lastYear]["Item"]
-    for group in groups:
-        data[group] = []
-    years = sorted(pceData.keys())
-    for year in years:
-        groupedData = pceData[year][field]
-        for i in range(len(groupedData)):
-            data[groups[i]].append(groupedData[i])
-
     fig = plt.figure(field, figsize=(9,6))
     ax = plt.subplot(111)
     ax.set_title(field)
     x_formatter = mpl.ticker.ScalarFormatter(useOffset=False)
     ax.xaxis.set_major_formatter(x_formatter)
     legendNames = []
-    for group in groups:
-        ax.plot(years, data[group])
+    for group in pceData:
+        ax.plot(allYears, percentifyField(field, group, pceData))
         legendNames.append(group)
     ax.legend(legendNames, loc=1)
     filename = "figures/" + field + ".png"
     plt.savefig(filename)
     print 'saved file to %s' % filename
-
-    compilationGroups = ["Top vs. Total", "Bottom vs. Total", "Top vs. Bottom"]
-    data["Top vs. Total"] = [(data[groups[5]][i] / data[groups[0]][i]) for i in range(len(years))]
-    data["Bottom vs. Total"] = [(data[groups[1]][i] / data[groups[0]][i]) for i in range(len(years))]
-    data["Top vs. Bottom"] = [(data[groups[5]][i] / data[groups[1]][i]) for i in range(len(years))]
-
-    fig = plt.figure(field, figsize=(9,6))
-    ax = plt.subplot(110)
-    ax.set_title(field)
-    x_formatter = mpl.ticker.ScalarFormatter(useOffset=False)
-    ax.xaxis.set_major_formatter(x_formatter)
-    legendNames = []
-    for group in compilationGroups:
-        ax.plot(years, data[group])
-        legendNames.append(group)
-    ax.legend(legendNames, loc=1)
-    filename = "figures/" + field + "_relative.png"
-    plt.savefig(filename)
-    print 'saved file to %s' % filename
-
 
 def printAllFields():
     pceData = loadPCEData()
@@ -130,12 +113,26 @@ def printAllFields():
     for field in listOfFields:
         print field
 
+def convertData(pceData):
+  """ Takes in Dict(year, Dict(field, List(percentile values))), gives out same data as
+      Dict(percentile name, Dict(field, List(year values)))) """
+  result = {}
+  groupNames = ["All", "0-20", "20-40", "40-60", "60-80", "80-100"]
+  for i in range(len(groupNames)):
+    groupDict = {}
+    for service in serviceNames:
+      serviceValues = []
+      for year in allYears:
+        serviceValues.append(pceData[year][service][i])
+      groupDict[service] = serviceValues
+    result[groupNames[i]] = groupDict
+  return result
+
 def main():
     pceData = loadPCEData()
-    incomeData = isolateIncome(pceData)
-    for field in pceData[lastYear]:
-      if (field != "Item") and (field != "Never attended and other"):
-        plotData(field, pceData)
+    cleanData = convertData(pceData)
+    for service in serviceNames:
+      plotData(service, cleanData)
 
 if __name__ == "__main__":
     main()
